9cb0f834290ec8691cad9a542fc95731f7608450
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2022-02-11 08:33:30 +0100

    SVG BUTTON HOVER

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,14 @@
 // import {example} from "./modules/example.js"
+
+// const svg = document.getElementsById("triangles");
+const svg = document.getElementById("triangles")
+
+svg.onclick = (e) => {
+  const colors = ["red","blue","green","orange", "pink","purple"]
+  const random = () => colors[Math.floor(Math.random() * colors.length)]
+  document.documentElement.style.cssText = ` 
+  --dark-color: ${random()};
+  --light-color: ${random()};
+  
+  `
+}
